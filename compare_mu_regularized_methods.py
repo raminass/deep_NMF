9ab208d5d81c224a8e74ad8dfc7b93905cb7da5e
YNWA <raminass@gmail.com>
2020-08-11 22:02:32 +0300

    adding error curve to L1 comparison

--- compare_mu_regularized_methods.py
+++ compare_mu_regularized_methods.py
@@ -1,6 +1,8 @@
 from utils import *
 import pandas as pd
 import sklearn.decomposition as sc
+from matplotlib import pyplot as plt
+
 
 EPSILON = np.finfo(np.float32).eps
 
@@ -24,24 +26,34 @@ H_init = initialize_exposures(V, n_components, method="ones")  # (n,k)
 H = H_init.copy()
 C = initialize_exposures(V, n_components, method="ones")
 lam = 1.5
-
+error_hoyer = []
 for i in range(200):
     nominator = np.dot(W.T, V)
-    denominator = np.dot(W.T.dot(W), H) + EPSILON*C + C * lam
+    denominator = np.dot(W.T.dot(W), H) + EPSILON * C + C * lam
     delta = nominator / denominator
     H *= delta
+    error_hoyer.append(frobenius_reconstruct_error(V, W, H))
 
-print('Hoyer method: ', frobenius_reconstruct_error(V, W, H))
+print('Hoyer method: ', error_hoyer[-1])
 
 # Our method
 H = H_init.copy()
 C = initialize_exposures(V, n_components, method="ones")
 lam = 1.5
+our_error = []
 
 for i in range(200):
     nominator = np.dot(W.T, V) - C * lam
-    denominator = np.dot(W.T.dot(W), H) + EPSILON*C
+    denominator = np.dot(W.T.dot(W), H) + EPSILON * C
     delta = nominator / denominator
     H *= delta
+    our_error.append(frobenius_reconstruct_error(V, W, H))
 
-print('our method: ', frobenius_reconstruct_error(V, W, H))
+print('our method: ', our_error[-1])
+
+
+plt.semilogy(error_hoyer, '-*', label='Hoyer')
+plt.semilogy(our_error, '-*', label='Our')
+plt.legend()
+plt.show()
+
